02a63505d36f1f35dc9fb6b96d5c62da8da86de9
Bikash888 <bikashdulal150@gmail.com>
2020-12-24 18:01:25 +0545

    header added

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -1,24 +1,52 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Styles from './Header.module.css';
 import Link from 'next/link';
 import 'antd/dist/antd.css'; 
 import {MenuOutlined} from '@ant-design/icons';
 
 const Header = () => {
+    const [showMenu,setShowMenu]=useState(true);
+    useEffect(()=>{
+        window.addEventListener('resize',navbarUpdate)
+    },[])
+    
+    
+    const navbarUpdate=()=>{
+        console.log(window.innerWidth)
+        if(window.innerWidth<750){
+            setShowMenu(false)
+        }else{
+            setShowMenu(true)
+        }
+    }
+
+    const closeNavbarHandler=()=>{
+        console.log(showMenu)
+        if(showMenu===false){
+            setShowMenu(true)
+        }
+        else{
+            setShowMenu(false)
+        }
+        
+       
+    }
     return (
         <div className={Styles.wrapper}>
             <div>
              Logo
             </div>
-              <div className={Styles.navWrapper}>
-                  <Link href='#'><a>Home</a></Link>
-                  <Link href='#'><a>About Us</a></Link>
-                  <Link href='#'><a>Post Event</a></Link>
-                  <Link href='#'><a>Contact Us</a></Link>
-                  <div className={Styles.menuWrapper}>
+            {showMenu?<div className={Styles.navWrapper}>
+                <Link href='#'><a>Home</a></Link>
+                <Link href='#'><a>About Us</a></Link>
+                <Link href='#'><a>Post Event</a></Link>
+                <Link href='#'><a>Contact Us</a></Link>
+                
+            </div>:null}
+             
+              <div className={Styles.menuWrapper} onClick={closeNavbarHandler}>
                   <MenuOutlined  className={Styles.menuIcon}/>
                   </div>
-              </div>
         </div>
     )
 }
